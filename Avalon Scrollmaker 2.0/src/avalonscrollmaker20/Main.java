e87043aac37c474e9521e51dab1d3d6a5714aa78
Michael Weigle <michael.weigle@gmail.com>
2010-05-05 07:47:34 +0000

    Initial project creation (Netbeans 6.8)

---
+++ Avalon Scrollmaker 2.0/src/avalonscrollmaker20/Main.java
@@ -0,0 +1,16 @@
+package avalonscrollmaker20;
+
+/**
+ *
+ * @author mw2xx
+ */
+public class Main
+{
+
+  public static void main( String[] args )
+  {
+    // Pop open display window.
+    // Stuff
+    // Exit
+  }
+}
